62503a1cac5cefcf73bc213eba850a16f83738f5
Nalabalachandravamsi <chandravamsinalabala223@gmail.com>
2024-08-01 19:10:25 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#include "ADC.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  port_intialization();
+  temp_sensor_intialization();
+  result();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
